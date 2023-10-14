54843a5d3a3e44cb6f03e2e926647452a5d64d31
q79843603 <timicq303@hotmail.com.hk>
2023-10-14 19:22:41 +0800

    second

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
